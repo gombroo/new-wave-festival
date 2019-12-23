db3dcaa9adcece5efeca08b908e5319d1bca7ba5
Ola Siem <info@siemieniuk.net>
2019-12-23 17:34:51 +0100

    Add middleware and database

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 
+// create server
 const app = express();
 
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+// database
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+];
+
+
+// http port
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
