712e52a1361712795162abb64d5c7c6a522a6dd8
Ola Siem <info@siemieniuk.net>
2020-01-07 09:11:05 +0100

    Fix localhost:3000 loading problem, update node and packages

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -5,12 +5,50 @@ const db = require('./../db');
 // GET /concerts
 router.route('/concerts').get((req, res) => {
   res.json(db.concerts);
-  console.log(concerts); 
 });
 
 // GET /concerts/:id
+router.route('/concerts/:id').get((req, res) => {
+  let newDb = db.concerts.filter((item) => {  // item = single db object
+    return item.id == req.params.id;
+  })
+  res.json(newDb);
+});
+
 // POST /concerts
-// DELETE /concerts/:id
+router.route('/concerts/').post((req, res) => {
+  const { author, text } = req.body;
+  db.concerts.push({id: (db.concerts[db.concerts.length -1].id +1) ,author, text});
+  // res.json(db.concerts); // show db with added item
+  res.send({ message: 'OK' }); // show message only
+});
+
 // PUT /concerts/:id
+router.route('/concerts/:id').put((req, res) => {
+  const { author, text } = req.body;  
+  const { id } = req.params;
+
+  db.concerts.map((item) => {
+    if(item.id == id){
+      item.author = author;
+      item.text = text;
+      return item;
+    }
+    return item;
+  });
+  res.send({ message: 'OK' }); // show message only 
+  // res.json(db.concerts); // show modified db
+});
+
+
+// DELETE /concerts/:id
+router.route('/concerts/:id').delete((req, res) => {
+  const { id } = req.params;
+  db.concerts = db.concerts.filter((item) => {
+    return item.id != id;
+  })
+  // res.json(db.concerts); // show new db with deleted item
+  res.send({ message: 'OK' }); // show message only
+});
 
 module.exports = router;
--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -7,9 +7,40 @@ router.route('/seats').get((req, res) => {
   res.json(db.seats);
 });
 
-// GET /seats/:id
 // POST /seats
+router.route('/seats/').post((req, res) => {
+  const { author, text } = req.body;
+  db.seats.push({id: (db.seats[db.seats.length -1].id +1) ,author, text});
+  // res.json(db.seats); // show db with added item
+  res.send({ message: 'OK' }); // show message only
+});
+
+// PUT /seats/:id
+router.route('/seats/:id').put((req, res) => {
+  const { author, text } = req.body;  
+  const { id } = req.params;
+
+  db.seats.map((item) => {
+    if(item.id == id){
+      item.author = author;
+      item.text = text;
+      return item;
+    }
+    return item;
+  });
+  res.send({ message: 'OK' }); // show message only 
+  // res.json(db.seats); // show modified db
+});
+
+
 // DELETE /seats/:id
-// PUT /seats/id
+router.route('/seats/:id').delete((req, res) => {
+  const { id } = req.params;
+  db.seats = db.seats.filter((item) => {
+    return item.id != id;
+  })
+  // res.json(db.seats); // show new db with deleted item
+  res.send({ message: 'OK' }); // show message only
+});
 
 module.exports = router;
